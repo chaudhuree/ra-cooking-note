e68df9fa092f51ab8943e20018ffbdc3ac9cec1d
chaudhuree <chaudhuree00@gmail.com>
2022-07-28 17:20:04 +0600

    docs: ingredients add on create page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // styles
 import './Create.css'
 
-export default function Create() {  
+export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
-  
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
+
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(title, method, cookingTime)
   }
-
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing=newIngredient.trim()
+    if(ing && !ingredients.includes(ing)){
+      setIngredients(prevIngredients=>[...prevIngredients, ing])
+      // setIngredients([...ingredients, ing])
+      // ingredients.push(ing)
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
+  }
   return (
     <div className="create">
       <h2 className="page-title">Add a New Recipe</h2>
@@ -20,8 +33,8 @@ export default function Create() {
 
         <label>
           <span>Recipe title:</span>
-          <input 
-            type="text" 
+          <input
+            type="text"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
             required
@@ -29,10 +42,24 @@ export default function Create() {
         </label>
 
         {/* recipe ingredients here */}
+        <label>
+          <span>Recipe Ingredients:</span>
+          <div className="ingredients">
+            <input
+              type="text"
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className="btn">add</button>
+          </div>
+        </label>
+        <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
+
 
         <label>
           <span>Recipe Method:</span>
-          <textarea 
+          <textarea
             onChange={(e) => setMethod(e.target.value)}
             value={method}
             required
@@ -41,11 +68,11 @@ export default function Create() {
 
         <label>
           <span>Cooking time (minutes):</span>
-          <input 
-            type="number" 
+          <input
+            type="number"
             onChange={(e) => setCookingTime(e.target.value)}
             value={cookingTime}
-            required 
+            required
           />
         </label>
 
