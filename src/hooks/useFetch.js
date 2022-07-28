f3fdf7d39979c9c87e02e55e0c56384ef87eefee
chaudhuree <chaudhuree00@gmail.com>
2022-07-28 19:24:16 +0600

    docs: create recipe

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (url,method='GET') => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
+  const [options, setOptions] = useState(null)
 
+  const postData = (postData) => {
+    setOptions({
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
   useEffect(() => {
     const controller = new AbortController()
 
-    const fetchData = async () => {
+    const fetchData = async (fetchOptions) => {
       setIsPending(true)
       
       try {
-        const res = await fetch(url, { signal: controller.signal })
+        const res = await fetch(url, { ...fetchOptions,signal: controller.signal })
         if(!res.ok) {
           throw new Error(res.statusText)
         }
@@ -30,14 +40,19 @@ export const useFetch = (url) => {
         }
       }
     }
+    if(method==='GET'){
+      fetchData()
+    }
+    if(method==='POST' && options){
 
-    fetchData()
+    fetchData(options)
+    }
 
     return () => {
       controller.abort()
     }
 
-  }, [url])
+  }, [url,options,method])
 
-  return { data, isPending, error }
+  return { data, isPending, error, postData }
 }
